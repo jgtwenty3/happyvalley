a0b5024037fe0952115e10213ab5452bda2e28ee
Justin <justinguerrerowork@gmail.com>
2024-12-14 21:39:42 -0500

    abduction animation working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Hero = () => {
+  const manRef = useRef(null);
+
+  useGSAP(() => {
+     gsap.to(manRef.current, { 
+      y: -200, 
+      opacity: 0, 
+      duration: 3, 
+      repeat: -1,
+      scrollTrigger: { 
+        trigger: manRef.current, 
+        start: "top 90%", 
+        end: "top 40%", 
+        scrub: true, 
+        onEnter: () => gsap.to(manRef.current, { opacity: 1 }),
+         onLeave: () => gsap.to(manRef.current, { opacity: 0 }), 
+        }, 
+      });
+     }, { scope: manRef });
+
   return (
     <div className='relative flex justify-center items-center w-full h-auto'>
       <img src="/logo.webp" alt="logo" className="w-auto max-w-full h-96" />
+      <div className="absolute bg-happySky w-[15%] h-36 top-[55%] left-[] md:w-[6%] md:h-[40%] md:top-[50%] lg:w-[4.5%] lg:h-44 lg:top-[45%]">
+        <img ref = {manRef} src = "/man.png" alt="man" className="absolute top-[100%] left-[36%] transform -translate-x-1/2 -translate-y-1/2 max-w-full h-auto lg:top-[100%] lg:left-[40%] lg:max-w-[110%] lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2"/>
+      </div>
       <img src="/ufo.webp" alt="ufo" className="absolute top-[31%] left-[29%] transform -translate-x-1/2 max-w-[40%] sm:top-[29%] sm:left-[40%] sm:max-w-[30%] md:top-[5%] md:left-[50%] md:max-w-[20%] lg:top-[29%] lg:left-[42%] lg:max-w-[15%] vibrate-1" />
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+const navItems = [
+  'HOME',
+  'GAMES',
+  'MENU',
+  'EVENTS',
+  'PICS',
+  'STORE',
+  'PARTY INQUIRY'
+];
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav className="bg-gray-800 text-white text-2xl p-4 rounded-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold"></div>
+        <div className="hidden md:flex space-x-4">
+          {navItems.map((item, index) => (
+            <a key={index} href="#" className="hover:text-gray-400">{item}</a>
+          ))}
+        </div>
+        <div className="md:hidden">
+          <button className="focus:outline-none">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
